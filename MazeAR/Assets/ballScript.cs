c608c12c2d8dd411f32505a2bcab1f5caee76962
calebcram <49875750+calebcram@users.noreply.github.com>
2020-02-26 02:22:19 -0700

    MazeAR
    Currently the ball is glitching above the stage, but everything else appears to be working correctly.

---
+++ MazeAR/Assets/ballScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballScript : MonoBehaviour
+{
+    public GameObject floor;
+    public GameObject spawnPoint;
+    //Allows the reset point for the game to be moved closer or further from the bottomm of the stage
+    public int RespawnDistance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Sets the Spawn point and the plane for the ball
+        //Respawns the ball if it falls below the maze
+        if(transform.position.y < floor.transform.position.y - RespawnDistance)
+        {
+            transform.position = spawnPoint.transform.position;
+        }
+    }
+}
